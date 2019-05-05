5dffdccaa03eb6a43a7d5b4fe1984c0a21f386bf
Justin Mozley <jtmozley@gmail.com>
2019-05-05 11:35:55 -0500

    initial setup of app

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
