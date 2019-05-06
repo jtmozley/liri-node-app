c1162816a167931f73c759e6f24ce1f48210648f
Justin Mozley <jtmozley@gmail.com>
2019-05-06 15:28:09 -0500

    Periodic Commit
    Commit for tutoring

--- liri.js
+++ liri.js
@@ -1,9 +1,25 @@
 require("dotenv").config();
+var axios = require('axios');
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-switch (key) {
-    case value:
+//var category = process.argv[2];
+var request = process.argv[2];
+
+var bandsUrl = "https://rest.bandsintown.com/artists/" + request + "/events?app_id=codingbootcamp"
+
+axios.get(bandsUrl)
+switch (category) {
+    case 'concert-this':
+        
+        break;
+    case 'spotify-this-song':
+        
+        break;
+    case 'movie-this':
+        
+        break;
+    case 'do-what-it-says':
         
         break;
 
